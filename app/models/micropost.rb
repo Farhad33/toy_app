251c7701a778325cb97bd6152206da4d9c03d5c4
Majid Rahimi <majid88rahimi@gmail.com>
2015-02-17 13:06:32 +0000

    Finish toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ActiveRecord::Base
     belongs_to :user
-    validates :content, length: { maximum: 140 }
+    validates :content, length: { maximum: 140 }, presence: true
+    validates :content, length: { minimum: 1 }, presence: true
+    validates :user_id, length: { minimum: 1 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
     has_many :microposts
+    validates :name, length: { minimum: 1 }, presence: true
+    validates :email, length: { minimum: 1 }, presence: true
 end
